07ebe0f4effaa8ccedb6d90a1a71fe981d76657a
Harman Deep Singh <115714095+Harman8815@users.noreply.github.com>
2024-02-12 11:32:07 +0530

    Add files via upload

---
+++ JAVA-Swing/Sorting-Algorithm-Visualizer-GUI-main/README.md
@@ -0,0 +1,41 @@
+# 📊 Project Title: Java Sorting Algorithm GUI
+
+## 📝 Description
+
+This GitHub repository contains the source code for a Java Sorting Algorithm GUI. The project aims to provide a graphical user interface (GUI) for visualizing various sorting algorithms implemented in Java using the Swing library. 🚀
+
+## Java Sorting Algorithm GUI
+
+<div style="display: flex;">
+    <img src="https://github.com/Harman8815/Sorting-Algorithm-Visualizer-GUI/assets/115714095/edd91eb5-4808-420a-a22a-ad5760cbf61e" width="250" height="350" style="margin-right: 10px;" />
+    <img src="https://github.com/Harman8815/Sorting-Algorithm-Visualizer-GUI/assets/115714095/8642554f-12f9-4770-897f-6130771fc4f8" width="550" height="350" />
+</div>
+
+https://github.com/Harman8815/Sorting-Algorithm-Visualizer-GUI/assets/115714095/2ea715e7-6568-4471-85d1-94cc15736a42
+
+## 🤵 Owner
+
+- **Name:** Harman Deep Singh
+- **GitHub:** [Harman88157](https://github.com/Harman88157)
+
+## 🚀 Technologies Used
+
+- **Language:** Java ☕
+- **Other Technologies:** JAVA Swing 🎨
+
+## ⚙️ Setup Instructions
+
+**Prerequisites:**
+   - You must have JDK installed on your system.
+   - Simply compile and run the `main.java` file. ⚙️
+   - And you are good to go.
+
+## 🛠️ How to Use
+
+1. Run the Java Sorting Algorithm GUI.
+2. Select a sorting algorithm from the dropdown menu.
+3. Click the "Start" button to visualize the sorting process.
+4. Observe the sorting algorithm in action through the graphical representation.
+5. Analyze the sorted output displayed on the screen.
+
+Explore and understand various sorting algorithms with the Java Sorting Algorithm GUI!
---
+++ JAVA-Swing/Sorting-Algorithm-Visualizer-GUI-main/Sorting_GUI_project/src/logic/logic.java
@@ -0,0 +1,204 @@
+package logic;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+
+import javax.swing.SwingUtilities;
+
+import ui.CustomPanel;
+import ui.main;
+
+public class logic extends javax.swing.JFrame  {
+
+	private static final long serialVersionUID = 1L;
+	int index=0;
+	static logic frame;
+	Thread mainThread;
+    public boolean sleeping=false;
+    public logic(int i) {
+    	mainThread = Thread.currentThread();
+        index=i;
+        index++;
+        initComponents();
+    }
+    private void initComponents() {
+
+        jPanel1 = new CustomPanel(this,index,1);
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        
+        setLocation(250,100);
+        setBackground(Color.BLACK);
+        setUndecorated(true);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1000, 600));
+        setMinimumSize(new java.awt.Dimension(1000, 600));
+        setPreferredSize(new java.awt.Dimension(1000, 600));
+        setTitle("Visualizer Screen");
+        
+        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 524, Short.MAX_VALUE)
+        );
+
+        jButton1.setBackground(new java.awt.Color(204, 51, 0));
+        jButton1.setFont(new java.awt.Font("Old English Text MT", 0, 24)); // NOI18N
+        jButton1.setText("Exit");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/prev.png"))); // NOI18N
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jButton3.setFont(new java.awt.Font("Old English Text MT", 0, 18)); // NOI18N
+        jButton3.setText("Start");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jButton4.setFont(new java.awt.Font("Old English Text MT", 0, 18)); // NOI18N
+        jButton4.setText("Reset");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/next.png"))); // NOI18N
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2.setBackground(Color.BLACK);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(175, 175, 175)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton5)
+                .addGap(17, 17, 17))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        pack();
+    }
+    protected void jButton5ActionPerformed(ActionEvent evt) {
+		// TODO Auto-generated method stub
+    	jPanel1.delaydecrease();
+    	
+	}
+	protected void jButton4ActionPerformed(ActionEvent evt) {
+		// TODO Auto-generated method stub
+		setVisible(false);
+    	new logic(index-1).setVisible(true);
+	}
+	protected void jButton3ActionPerformed(ActionEvent evt) {
+		// TODO Auto-generated method stub
+		jPanel1.i=1;
+		System.out.println("hii");
+	}
+	private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {         
+    	jPanel1.delayincrease();
+    }               
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {         
+    	setVisible(false);
+    	new main().setVisible(true);
+    	System.out.print("EXIT");
+    }                                        
+
+    public static void main(String args[]) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(logic.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(logic.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(logic.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(logic.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    
+
+        SwingUtilities.invokeLater(() -> {
+            frame = new logic(1);
+           frame.setVisible(true);
+       });
+    }
+                  
+    
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private CustomPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    // End of variables declaration                   
+}
